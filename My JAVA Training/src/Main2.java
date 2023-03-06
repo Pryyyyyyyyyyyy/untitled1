0be19c07a6279d53cd89f04abeeec7b804a1234b
Pryyyyyyyyyyyy <anna27pr@gmail.com>
2023-03-06 12:39:12 +0200

    added homework

---
+++ My JAVA Training/src/Main2.java
@@ -0,0 +1,33 @@
+class Main3 {
+    public static void main(String[] args) {
+       /* int a = 7;
+        int b = 9;
+        a = a + b; // a = 7 + 9 = 16
+        b = a - b; // b = 16 - 9 = 7
+        a = a - b; // a = 16 - 7 = 9
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+*/
+      /*  double chislitel = 7.0;
+        double znamenatel = 5.0;
+        int celayaChast = (int) (chislitel / znamenatel);
+        int ostatok = (int) (chislitel % znamenatel);
+        int znamenatelInt = (int) znamenatel;
+        System.out.println("Целая часть равна - " + celayaChast);
+        System.out.println("Остаток от деления равен - " + ostatok);
+        System.out.println("Знаменатель равен - " + znamenatelInt);*/
+
+       /* String string1 = "This line that i want to cut, cause it is too long";
+        String string2 = string1.substring(0, 35);
+        String string3 = string2.substring(0, 17) + "don't want to cut, cause it is perfect";
+        System.out.println("Строка 1: " + string1);
+        System.out.println("Длина строки 1: " + string1.length());
+        System.out.println("Строка 2: " + string2);
+        System.out.println("Длина строки 2: " + string2.length());
+        System.out.println("Строка 3: " + string3);
+        System.out.println("Длина строки 3: " + string3.length());*/
+
+
+
+    }
+}
