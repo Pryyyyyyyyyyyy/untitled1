da28f26d2f5e2378b73a634b8b682722821d9dbd
Pryyyyyyyyyyyy <anna27pr@gmail.com>
2023-03-06 13:08:21 +0200

    added homework

--- My JAVA Training/src/Main2.java
+++ My JAVA Training/src/Main2.java
@@ -27,7 +27,77 @@ class Main3 {
         System.out.println("Строка 3: " + string3);
         System.out.println("Длина строки 3: " + string3.length());*/
 
+       /* String string = "Testing, is my favourite job";
 
+        // Разбиваем строку на список слов
 
+        String[] words = string.split(" ");*/
+
+        // Выводим каждое слово и его длину
+
+       /* for (int i = 0; i < words.length; i++) {
+            String word = words[i].replaceAll(",", "");
+            System.out.printf("Слово%d = %s, Длина этого слова = %d%n", i+1, word, word.length());
+        }
+
+        // Проверяем, является ли первое слово длиннее остальных
+        int firstWordLength = words[0].replaceAll(",", "").length();
+        int[] otherWordLengths = new int[words.length-1];
+        for (int i = 1; i < words.length; i++) {
+            otherWordLengths[i-1] = words[i].replaceAll(",", "").length();
+        }
+        int maxOtherWordLength = 0;
+        for (int i = 0; i < otherWordLengths.length; i++) {
+            if (otherWordLengths[i] > maxOtherWordLength) {
+                maxOtherWordLength = otherWordLengths[i];
+            }
+        }
+        if (firstWordLength > maxOtherWordLength) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+*/
+
+/*                String text = "Completely random text in English. In it, we just need to determine how many times the character 'a' occurs there. And we can use the split method and the length method.";
+
+                // Split the text into an array of words
+                String[] words = text.split("\\s+");
+
+                // Initialize a counter variable
+                int count = 0;
+
+                // Loop through each word in the array
+                for (String word : words) {
+                    // Loop through each character in the word
+                    for (int i = 0; i < word.length(); i++) {
+                        // Check if the character is 'a' or 'A'
+                        if (word.charAt(i) == 'a' || word.charAt(i) == 'A') {
+                            count++;
+                        }
+                    }
+                }
+
+                // Print the count of 'a' characters in the text
+                System.out.println("The text contains " + count + " 'a' characters.");*/
+/*
+        String text = "Completely random text in English. In it, we just need to determine how many times the character 'a' occurs there. And we can use the split method and the length method.";
+
+        // Convert the text to lowercase
+        String lowerCaseText = text.toLowerCase();
+
+        // Initialize a counter variable
+        int count = 0;
+
+        // Loop through each character in the lowercase text
+        for (int i = 0; i < lowerCaseText.length(); i++) {
+            // Check if the character is 'a'
+            if (lowerCaseText.charAt(i) == 'a') {
+                count++;
+            }
+        }
+
+        // Print the count of 'a' characters in the text
+        System.out.println("The text contains " + count + " 'a' characters.");*/
     }
 }
