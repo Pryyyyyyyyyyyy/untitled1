83fe6d6541af44e2a589a26a0465dcc415d78201
Pryyyyyyyyyyyy <anna27pr@gmail.com>
2023-03-28 23:04:57 +0300

    Merge remote-tracking branch 'origin/main'

--- My JAVA Training/Homework 20/src/Alkash.java
+++
@@ -1,9 +0,0 @@
-
-public class Alkash {
-    public static void main(String[] args) {
-        Seller seller = new Seller("Мавка", true);
-        Buyer buyer = new Buyer("Кицюня", 25, true);
-        Shop shop = new Shop("Генделик");
-        shop.returnCost(seller, buyer, "alcohol drink", 10.0);
-    }
-}
