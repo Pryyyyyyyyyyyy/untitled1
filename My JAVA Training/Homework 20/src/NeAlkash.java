7073a52e9d883ff3d36b050f9502ecf312a3e7ec
Pryyyyyyyyyyyy <125798736+Pryyyyyyyyyyyy@users.noreply.github.com>
2023-03-28 23:03:41 +0300

    11111

--- My JAVA Training/Homework 20/src/NeAlkash.java
+++
@@ -1,9 +0,0 @@
-
-public class NeAlkash {
-    public static void main(String[] args) {
-        Seller seller = new Seller("Мавка", true);
-        Buyer buyer = new Buyer("Зюзя", 18, true);
-        Shop shop = new Shop("Генделик");
-        shop.returnCost(seller, buyer, "drink", 10.0);
-    }
-}
